b79ee58392a82dc6c151540e592b55f3766029e5
Tatsushi Kobayashi <tatsushi.kobayashi15@outlook.jp>
2017-03-20 07:10:10 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'top/index'
+
+  get 'contacts' => 'contacts#new'
+  resources :contacts, only: [:index, :new, :create, :edit, :update ,:destroy] do
+    collection do
+      post :confirm
+    end
+  end
+  
+  resources :blogs, only: [:index, :new, :create, :edit, :update ,:destroy] do
+    collection do
+      post :confirm
+    end
+  end
+
+  root 'top#index'
+end
